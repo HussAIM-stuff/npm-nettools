eacd2707a86a397651644b249ee402bd1b2f901c
huss <hussein.odeh@gmail.com>
2021-02-12 09:51:54 +0100

    start1.1

--- index.js
+++ index.js
@@ -10,35 +10,34 @@ const defaults = {
   env: null
 };
 
-exports.test1 = () => {
-  exec('cat *.js bad_file | wc -l', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-  });
-}
-
-
-// exports async function lsAll() {
-//   const { stdout, stderr } = await execP('ls -al');
-//   console.log('stdout:\n', stdout.split('\n').map(el => {
-//     if(el[0] === 'd' || el[0] === '-') {
-//       let aux = el.split(' ');
-//       return {
-//         name: aux[8],
-//         lastMod: aux.slice(5,8),
-//         size: aux[4],
-//         user: aux[2],
-//         group: aux[3],
-//         permissions: aux[0],
-//         unknown: aux[1]
-
-//       };
+// exports.test1 = () => {
+//   exec('cat *.js bad_file | wc -l', (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`exec error: ${error}`);
+//       return;
 //     }
-//   }).filter(el => el));
-//   console.log('stderr:\n', stderr);
+//     console.log(`stdout: ${stdout}`);
+//     console.log(`stderr: ${stderr}`);
+//   });
 // }
-// lsAll();
+
+
+exports async function lsAll() {
+  const { stdout, stderr } = await execP('ls -al');
+  console.log('stdout:\n', stdout.split('\n').map(el => {
+    if(el[0] === 'd' || el[0] === '-') {
+      let aux = el.split(' ');
+      return {
+        name: aux[8],
+        lastMod: aux.slice(5,8),
+        size: aux[4],
+        user: aux[2],
+        group: aux[3],
+        permissions: aux[0],
+        unknown: aux[1]
+
+      };
+    }
+  }).filter(el => el));
+  console.log('stderr:\n', stderr);
+}
